e5881db60351240ebd3cbfd3b63f24cf7a8cb3a2
Dakrfox <Relibarion@gmail.com>
2022-04-29 18:36:31 -0500

    freelancer-rates

---
+++ freelancer-rates/freelancer-rates.js
@@ -0,0 +1,60 @@
+// @ts-check
+//
+// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion on the web
+// and supported IDEs when implementing this exercise. You don't need to
+// understand types, JSDoc, or TypeScript in order to complete this JavaScript
+// exercise, and can completely ignore this comment block and directive.
+
+// 👋🏽 Hi again!
+//
+// A quick reminder about exercise stubs:
+//
+// 💡 You're allowed to completely clear any stub before you get started. Often
+// we recommend using the stub, because they are already set-up correctly to
+// work with the tests, which you can find in ./freelancer-rates.spec.js.
+//
+// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
+// in idiomatic JavaScript, but some companies and style-guides do enforce them.
+//
+// Get those rates calculated!
+const DAILY_HOURS = 8;
+/**
+ * The day rate, given a rate per hour
+ *
+ * @param {number} ratePerHour
+ * @returns {number} the rate per day
+ */
+export function dayRate(ratePerHour) {
+  return ratePerHour*DAILY_HOURS;
+}
+
+/**
+ * Calculates the number of days in a budget, rounded down
+ *
+ * @param {number} budget: the total budget
+ * @param {number} ratePerHour: the rate per hour
+ * @returns {number} the number of days
+ */
+export function daysInBudget(budget, ratePerHour) {
+  return Math.floor(budget / dayRate(ratePerHour))
+}
+
+/**
+ * Calculates the discounted rate for large projects, rounded up
+ *
+ * @param {number} ratePerHour
+ * @param {number} numDays: number of days the project spans
+ * @param {number} discount: for example 20% written as 0.2
+ * @returns {number} the rounded up discounted rate
+ */
+export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+  let num_months = Math.floor(numDays / 22);
+  let month_rate = 22 * dayRate(ratePerHour);
+  let discount_p = (1 - discount) * month_rate;
+
+  let extra_days = numDays % 22;
+  let p_extra_days = extra_days * dayRate(ratePerHour);
+
+  return Math.ceil(num_months * discount_p + p_extra_days);
+}
