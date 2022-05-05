85410e83c963bd2496db1729d76ce2bcc9a37d8a
Dakrfox <Relibarion@gmail.com>
2022-05-04 23:56:27 -0500

    mixed juices

--- mixed-juices/mixed-juices.js
+++ mixed-juices/mixed-juices.js
@@ -11,26 +11,33 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-  switch(name){
-    case 'Pure Strawberry Joy':
+
+/**
+ * switch (name) {
+  case 'Pure Strawberry Joy':
       return 0.5;
-    break;
-    case 'Energizer':
+  break;
+  case 'Energizer' || name == 'Green Garden':
       return 1.5;
-    break;
-    case 'Green Garden':
-      return 1.5;
-    break;
-    case 'Tropical Island':
+  break;
+  case 'Tropical Island':
       return 3;
-    break;
-    case 'All or Nothing':
+  break;
+  case 'All or Nothing':
       return 5;
+  break;
+  default:
+    return 2.5;
     break;
-    default:
-      return 2.5;
-    break;
-  }
+}
+ */
+
+
+  if (name == 'Pure Strawberry Joy') return 0.5;
+  if (name == 'Energizer' || name == 'Green Garden') return 1.5;
+  if (name == 'Tropical Island') return 3;
+  if (name == 'All or Nothing') return 5;
+  return 2.5;
 }
 
 /**
@@ -42,7 +49,21 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Please implement the limesToCut function');
+  
+  let counter = 0;
+  let index = 0;
+  while (index < limes.length ) {
+    if(wedgesNeeded == 0) return 0;
+    if (limes[index] == 'small') counter +=6;
+    if (limes[index] == 'medium') counter +=8;
+    if (limes[index] == 'large') counter +=10;
+    index++;
+    if(counter >= wedgesNeeded) break;
+  }
+  return index;
+
+
+
 }
 
 /**
@@ -53,5 +74,12 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Please implement the remainingOrders function');
+  let index = 0;
+  while (orders.length != 0){
+    orders.shift();
+    timeLeft -= timeToMixJuice(orders[index]) 
+    index++
+    if(orders.length == 0 || timeLeft <= 0) break;
+  }
+  return orders;
 }
